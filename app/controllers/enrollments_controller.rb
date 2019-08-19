fb42ba0f506904badd3e5a943c5a275e90577624
Jeremy Oglesby <iaminaband@gmail.com>
2019-08-19 11:00:42 -0700

    Implements Stripe payments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,30 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!, :set_course
   
   def create
-    current_user.enrollments.create(course: @course)
+    if @course.premium?
+      # Amount in cents
+      @amount = (@course.cost * 100).to_i
 
+      # Stripe
+      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+      
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
+      
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premium Content',
+        currency: 'usd'
+      )
+    end
+    
+    current_user.enrollments.create(course: @course)
+    redirect_to course_path(@course)
+  rescue Stripe::CardError => error
+    flash[:error] = error.message
     redirect_to course_path(@course)
   end
 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_is_enrolled, only: [:show]
 
   # GET /lessons
   # GET /lessons.json
@@ -12,64 +14,16 @@ class LessonsController < ApplicationController
   def show
   end
 
-  # GET /lessons/new
-  def new
-    @lesson = Lesson.new
-  end
-
-  # GET /lessons/1/edit
-  def edit
-  end
-
-  # POST /lessons
-  # POST /lessons.json
-  def create
-    @lesson = Lesson.new(lesson_params)
-
-    respond_to do |format|
-      if @lesson.save
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
-        format.json { render :show, status: :created, location: @lesson }
-      else
-        format.html { render :new }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /lessons/1
-  # PATCH/PUT /lessons/1.json
-  def update
-    respond_to do |format|
-      if @lesson.update(lesson_params)
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lesson }
-      else
-        format.html { render :edit }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /lessons/1
-  # DELETE /lessons/1.json
-  def destroy
-    @lesson.destroy
-    respond_to do |format|
-      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
+  private #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # Use callbacks to share common setup or constraints between actions.
     def set_lesson
       # memoize @lesson to reduce database between new->create
       @lesson ||= Lesson.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def lesson_params
-      params.require(:lesson).permit(:title, :subtitle)
+    def ensure_user_is_enrolled
+      unless current_user.enrolled_in?(@lesson.section.course)
+        redirect_to course_path(@lesson.section.course), alert: "You must enroll in a course to view the lessons."
+      end
     end
 end
