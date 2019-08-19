214ed13ad0947a445b95c79d7e4319df61e6b67b
Jeremy Oglesby <iaminaband@gmail.com>
2019-08-19 12:18:38 -0700

    Adds support for ordering lessosn by rank

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :section
   validates :title, presence: true
   mount_uploader :video, VideoUploader
+
+  include RankedModel
+  ranks :row_order, with_same: :section_id
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,9 @@
 class Section < ApplicationRecord
   belongs_to :course
   has_many :lessons
+  attr_reader :number_of_lessons
+
+  def number_of_lessons
+    lessons.count
+  end
 end
